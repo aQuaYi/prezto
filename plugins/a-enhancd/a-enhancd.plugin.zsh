4ea570b8848a829dacd37754d115c8b5846e2b52
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-04-20 00:32:20 +0800

    fix enhancd bug

--- plugins/a-enhancd/a-enhancd.plugin.zsh
+++ plugins/a-enhancd/a-enhancd.plugin.zsh
@@ -1,5 +1,14 @@
 # Source module files.
 source "${0:h}/enhancd/init.sh" || return 1
 
+## 添加这么一段无聊的内容是因为
+# https://github.com/b4b4r07/enhancd/issues/123
+# 问题解决后，可以删除下面这段
+export ENHANCD_HOOK_AFTER_CD=''
+NOW_DIR=`pwd`
+cd $ZPREZTODIR/plugins/a-enhancd/enhancd
+git checkout .
+cd $NOW_DIR
+
 export ENHANCD_HOOK_AFTER_CD='exa --classify -a'
 
