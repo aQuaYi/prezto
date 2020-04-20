34b5be95e69456c7fe37b14342a5c883f002e7df
aQuaYi.com <6028869+aQuaYi@users.noreply.github.coml>
2020-04-20 13:13:21 +0800

    delete command mode

--- plugins/a-autosuggestions/a-autosuggestions.plugin.zsh
+++ plugins/a-autosuggestions/a-autosuggestions.plugin.zsh
@@ -3,7 +3,7 @@ pmodload 'editor'
 
 source ${0:A:h}/zsh-autosuggestions/zsh-autosuggestions.zsh || return 1
 
-export ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 bindkey '^j' autosuggest-execute # CTRL-j
 
